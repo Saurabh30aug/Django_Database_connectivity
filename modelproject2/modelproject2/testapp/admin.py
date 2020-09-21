c5bf33d070df3137053507aeca5e43f4751369da
Saurabh Srivastava <51634729+Saurabh30aug@users.noreply.github.com>
2020-09-21 15:24:24 +0530

    Add files via upload

---
+++ modelproject2/modelproject2/testapp/admin.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib import admin
+from testapp.models import Employee
+# Register your models here.
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display=['eno','ename','esal','eaddr']
+admin.site.register(Employee,EmployeeAdmin)
